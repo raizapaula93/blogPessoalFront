45ab620e611483a0ef98f8abe8b88073fe2b60cd
raizapaula93 <raizapaula93@gmail.com>
2022-04-01 02:09:31 -0300

    @raizapaula93[TASK: Componente inicio sem consumo]

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { UserLogin } from '../model/UserLogin';
+import { environment } from 'src/environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
@@ -19,6 +20,16 @@ export class AuthService {
 
   cadastrar(user:User):Observable<User>{
         return this.http.post<User>('https://blogpessoalraizadepaula.herokuapp.com/usuarios/cadastrar',user)
+  }
+
+  logado(){
+    let ok = false
+
+    if(environment.token != ''){
+      ok = true
+    }
+
+    return ok
   }
 
 }
