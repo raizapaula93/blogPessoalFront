bc17ab37c72a912fff3fb77cb14d96b6eda59d0b
raizapaula93 <raizapaula93@gmail.com>
2022-04-04 13:24:00 -0300

    @raizapaula93[Post tema erro]

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -15,11 +15,11 @@ export class AuthService {
   ) {}
 
   entrar(userLogin:UserLogin):Observable<UserLogin>{
-        return this.http.post<UserLogin>('https://blogpessoalraizadepaula.herokuapp.com/usuarios/logar',userLogin)
+        return this.http.post<UserLogin>('https://blogpessoalthiago.herokuapp.com/usuarios/logar',userLogin)
   }
 
   cadastrar(user:User):Observable<User>{
-        return this.http.post<User>('https://blogpessoalraizadepaula.herokuapp.com/usuarios/cadastrar',user)
+        return this.http.post<User>('https://blogpessoalthiago.herokuapp.com/usuarios/cadastrar',user)
   }
 
   logado(){
--- src/app/tema/tema.component.ts
+++ src/app/tema/tema.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { Tema } from '../model/Tema';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-tema',
@@ -9,13 +11,32 @@ import { environment } from 'src/environments/environment.prod';
 })
 export class TemaComponent implements OnInit {
 
+  tema: Tema = new Tema();
+  listaTemas:Tema[];
+
+
   constructor(
-    private route: Router) { }
-      
+    private router: Router,
+    private temaService: TemaService) { }
+
   ngOnInit(){
     if(environment.token==''){
-      this.route.navigate(['/entrar'])
-  }
-  }
+      this.router.navigate(['/entrar'])
+      }
+    }
 
+    findAllTemas(){
+      this.temaService.getAllTema().subscribe((resp: Tema[])=>{
+        this.listaTemas = resp
+      } )      
+    }
+
+   cadastrar(){
+      this.temaService.postTema(this.tema).subscribe((resp: Tema)=>{
+        this.tema = resp;
+        alert('Tema cadastrado com sucesso!');
+        this.findAllTemas();
+        this.tema = new Tema()
+      })
+    }
 }
