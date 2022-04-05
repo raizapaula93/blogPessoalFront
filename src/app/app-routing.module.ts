68ff7a54061bc87a90163dec0c2c84170468b619
raizapaula93 <raizapaula93@gmail.com>
2022-04-05 19:57:53 -0300

    @raizapaula93[PUT e DELETE CRUD]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
-import {CadastrarComponent} from './cadastrar/cadastrar.component';
+import { PostagemDeleteComponent } from './delete/postagem-delete/postagem-delete.component';
+import { PostagemEditComponent } from './edit/postagem-edit/postagem-edit.component';
+import { TemaDeleteComponent } from './delete/tema-delete/tema-delete.component';
+import { TemaEditComponent } from './edit/tema-edit/tema-edit.component';
+import { TemaComponent } from './tema/tema.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { EntrarComponent } from './entrar/entrar.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { InicioComponent } from './inicio/inicio.component';
-import { TemaComponent } from './tema/tema.component';
-import { TemaEditComponent } from './edit/tema-edit/tema-edit.component';
-import { TemaDeleteComponent } from './delete/tema-delete/tema-delete.component';
 
 const routes: Routes = [
 
@@ -13,10 +15,14 @@ const routes: Routes = [
 
 {path:'entrar', component: EntrarComponent},
 {path:'cadastrar', component: CadastrarComponent},
+
 {path: 'inicio', component:InicioComponent},
 {path: 'tema', component:TemaComponent},
+
 {path: 'tema-edit/:id', component:TemaEditComponent},
-{path: 'tema-delete/:id', component:TemaDeleteComponent}
+{path: 'tema-delete/:id', component:TemaDeleteComponent},
+{path: 'postagem-edit/:id', component: PostagemEditComponent},
+{path: 'postagem-delete/:id', component: PostagemDeleteComponent}
 
 ];
 
